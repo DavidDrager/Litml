710e827ab0aab903f84ad3b2e247afc9c01a45b4
DavidDrager <david@drager.me>
2021-01-31 17:59:38 -0500

    Remove shadow DOM from LitmlWork

--- litml-work.js
+++ litml-work.js
@@ -6,30 +6,21 @@ class LitmlWork extends HTMLElement {
 
     constructor() {
         super();
+/*         this.jsonLdNode = document.createElement('script');
+        this.jsonLdNode.setAttribute('type',"application/ld+json");
+        this.appendChild(this.jsonLdNode);
         this.initTemplate();
         this.attachShadow({ mode: 'open' });
         this.shadowRoot.appendChild(this.template.content.cloneNode(true));
-        this._slot = this.shadowRoot.querySelector('article slot');
+        this._slot = this.shadowRoot.querySelector('article slot'); */
     }
 
-    generateHeaderAndSchema() {
-        var authorName = null;
-        var title = null;    
+    generateHeader(workInfo) {
         var articleNode;
-        var workType;
-        var subtitle;
-        var workInfo;
         var headerHtml;
         var headerObj;
         var jsonLd;
 
-        authorName = this.getAttribute("litml-author");
-        title = this.getAttribute("litml-title"); 
-        workType = this.getAttribute("litml-work-type"); 
-        subtitle = this.getAttribute('litml-subtitle');
-        workInfo = { "authorName": authorName, "title": title, "subtitle": subtitle, "workType": workType };
-        articleNode = this.shadowRoot.querySelector('article');
-        
         if (this.publisherTemplates && this.publisherTemplates.workHeader) {
             headerHtml = this.publisherTemplates.workHeader(workInfo);
         }
@@ -40,14 +31,12 @@ class LitmlWork extends HTMLElement {
         if (headerHtml) {
             headerObj = document.createElement("header");
             headerObj.innerHTML = headerHtml;
-            articleNode.insertBefore(headerObj,this._slot);         
+            this.prepend(headerObj);         
         }
-
-        jsonLd = this.generateJsonLd(workInfo,articleNode);
     }
 
 
-    generateJsonLd(workInfo,article) {
+    generateJsonLd(workInfo) {
         var jsonLDTxt;
         var scriptNode;
 
@@ -78,13 +67,50 @@ class LitmlWork extends HTMLElement {
             '} }';
         }
 
-        scriptNode = article.querySelector("script");
+        scriptNode = this.querySelector("script");
         scriptNode.innerText = jsonLDTxt;
+        // this.jsonLdNode.innerText = this.jsonLdNode;
     }
+
+    buildWorkInfo() {
+        var authorName = null;
+        var title = null;    
+        var subtitle;
+        var workType;
+
+        authorName = this.getAttribute("litml-author");
+        title = this.getAttribute("litml-title"); 
+        workType = this.getAttribute("litml-work-type"); 
+        subtitle = this.getAttribute('litml-subtitle');
+        this.workInfo = { "authorName": authorName, "title": title, "subtitle": subtitle, "workType": workType };
+        return this.workInfo;
+    }
+
+    addContent()  {
+        this.buildWorkInfo();
+        this.generateHeader(this.workInfo);
+        
+        this.initTemplate();
+        this.prepend(this.template.content.cloneNode(true));
+        this.generateJsonLd(this.workInfo);
+    }
+
 
     connectedCallback() {
         if (this.isConnected) {
-            this.generateHeaderAndSchema();
+            this.addContent();
+
+/*             this.initTemplate();
+            this.prepend(this.template.content.cloneNode(true)); */
+            // this.attachShadow({ mode: 'open' });
+            // this.shadowRoot.appendChild(this.template.content.cloneNode(true));
+            // this._slot = this.shadowRoot.querySelector('slot');
+            
+/*             this.jsonLdNode = document.createElement('script');
+            this.jsonLdNode.setAttribute('type',"application/ld+json");
+            this.prepend(this.jsonLdNode);
+
+            this.generateHeaderAndSchema(); */
         }
     }
 }
@@ -101,7 +127,15 @@ LitmlWork.prototype.initTemplate = function() {
         '  "@context": "https://schema.org/", ' ;
 
         template = document.createElement('template');
+
         template.innerHTML =
+        '   <style> ' +
+        'litml-work { display: block;  }' +
+        'litml-work ([hidden]) {  display: none;  } ' +
+        '</style><script type="application/ld+json"></script>';
+
+
+/*         template.innerHTML =
         '   <style> ' +
         ':host { display: block;  }' +
         ':host([hidden]) {  display: none;  } ' +
@@ -111,38 +145,39 @@ LitmlWork.prototype.initTemplate = function() {
         ':host([litml-line-type="tab"]) {   --litml-tabs-num-work: 1;  --litml-text-align-work: left; }' +
         ':host([litml-line-type="hanging"]) {   --litml-tabs-num-work: -1;  --litml-text-align-work: left; }' +
         '</style>' + '<article><script type="application/ld+json"></script>' +
-        '<slot></slot></article>';
+        '<slot></slot></article>'; */
+
 
         LitmlWork.prototype.template = template;
-
-        LitmlWork.prototype.generateDefaultHeaderHtml =  function(workInfo) {
-            // var authorHObj;
-            // var titleHObj;
-            // var subtitleHObj;
-            // var headerObj;
-            var name = workInfo.title;
-            var headerHtml = ""
-    
-            if ( workInfo.author || name) {
-                // headerHtml = "<header>"
-    
-                if (name) {
-                    headerHtml = headerHtml + "<h1>" + name + "</h1>";
-                }
-    
-                if (workInfo.subtitle) {
-                    headerHtml = headerHtml + "<h3>" + workInfo.subtitle + "</h3>";
-                }
-    
-                if (workInfo.authorName) {
-                    headerHtml = headerHtml + "<h2>" + workInfo.authorName + "</h2>";
-                }
-                // headerHtml = headerHtml + "</header>";
-            }
-
-            return headerHtml;
-        }
     }
+};
+
+LitmlWork.prototype.generateDefaultHeaderHtml =  function(workInfo) {
+    // var authorHObj;
+    // var titleHObj;
+    // var subtitleHObj;
+    // var headerObj;
+    var name = workInfo.title;
+    var headerHtml = ""
+
+    if ( workInfo.author || name) {
+        // headerHtml = "<header>"
+
+        if (name) {
+            headerHtml = headerHtml + "<h1>" + name + "</h1>";
+        }
+
+        if (workInfo.subtitle) {
+            headerHtml = headerHtml + "<h3>" + workInfo.subtitle + "</h3>";
+        }
+
+        if (workInfo.authorName) {
+            headerHtml = headerHtml + "<h2>" + workInfo.authorName + "</h2>";
+        }
+        // headerHtml = headerHtml + "</header>";
+    }
+
+    return headerHtml;
 };
 
 LitmlWork.register = function(publisherInfo, publisherTemplates) {
